1889d1233ff4859e3cceb9142df654b4e4cd0149
Max Thayer <garbados@gmail.com>
2013-10-14 19:18:45 -0400

    begin re-organization

--- tasks/daemon.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (mount, remote) {
-  if (!mount || !remote) {
-    throw new TypeError();
-  } else {
-    console.log('you did it!');
-  }
-};
--- tasks/undaemon.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (mount, remote) {
-  console.log('you did it!');
-};
